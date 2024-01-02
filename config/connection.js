3dd9e55fe8aab394f66d4ceefb9cb958416ebd17
Fxkhalil0 <fxkhalil0@gmail.com>
2024-01-02 14:45:36 +0200

    ..

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+console.log('MongoDB URI:', uri);
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
---
+++ server.js
@@ -0,0 +1,30 @@
+require("./config/connection");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(
+  {
+      origin: ["https://lvw.onrender.com/"],
+      methods: ["POST", "GET"],
+      credentials: true
+  }
+));
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
+  res.send("Welcome");
+});
+
+const userRoute = require("./Routers/userRoute");
+app.use("/user", userRoute);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+server.listen(5000, function () {
+  console.log("listen");
+});
