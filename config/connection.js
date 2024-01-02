1713ed71403a11c6910d1b2c9a52b49e15698b50
Fxkhalil0 <fxkhalil0@gmail.com>
2024-01-03 00:01:21 +0200

    ..

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    await client.connect(uri,
+      options,
+      (err) => {
+       if(err) console.log(err) 
+       else console.log("mongdb is connected");
+      });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
