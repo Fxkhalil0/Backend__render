ff9b0743f96554d24ddfda808263bd62b020e1f2
Fxkhalil0 <fxkhalil0@gmail.com>
2024-01-03 16:33:42 +0200

    ..

--- server.js
+++ server.js
@@ -6,46 +6,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { MongoClient } = require('mongodb');
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const port = process.env.PORT || 5000
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header({ "Access-Control-Allow-Credentials": true });
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
-  res.header("Access-Control-Max-Age", 24 * 60 * 60 * 1000);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
-
-  next()
-})
-// app.use(
-//   cors({
-//     origin: "https://lvw.onrender.com", // Allow requests only from this origin
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//   })
-// );
-
-app.use(bodyParser.json());
-app.get("/", function (req, res) {
-  res.send("Welcome");
-});
-
-// CORS middleware for specific routes
-app.use("/user", cors({
-  origin: "https://lvw.onrender.com",
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
-const userRoute = require("./Routers/userRoute");
-app.use("/user", userRoute);
-
-
 // Middleware to establish MongoDB connection
 app.use(async (req, res, next) => {
   const client = new MongoClient(uri, {
@@ -62,6 +22,40 @@ app.use(async (req, res, next) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header({ "Access-Control-Allow-Credentials": true });
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
+  res.header("Access-Control-Max-Age", 24 * 60 * 60 * 1000);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
+
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("Welcome");
+});
+
+// CORS middleware for specific routes
+app.use("/user", cors({
+  origin: "https://lvw.onrender.com",
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+const userRoute = require("./Routers/userRoute");
+app.use("/user", userRoute);
+
 // Close MongoDB connection after handling all requests
 app.use((req, res) => {
   if (req.dbClient) {
@@ -69,8 +63,6 @@ app.use((req, res) => {
   }
   res.status(404).send('Not Found');
 });
-
-
 
 server.listen(port, function () {
   console.log("listen");
