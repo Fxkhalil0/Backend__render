c83c7e303f15899aba4373cc91af7543e93b19b3
Fxkhalil0 <fxkhalil0@gmail.com>
2024-01-03 13:15:05 +0200

    ..

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: "https://lvw.onrender.com", // Allow requests only from this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  })
+  cors()
 );
 app.use(bodyParser.json());
 app.get("/", function (req, res) {
