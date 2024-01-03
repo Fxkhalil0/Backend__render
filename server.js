07ea3da7a5c650be78715b8a40fbe3c23c43d362
Fxkhalil0 <fxkhalil0@gmail.com>
2024-01-03 15:04:25 +0200

    ..

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://lvw.onrender.com");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header({ "Access-Control-Allow-Credentials": true });
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
   res.header("Access-Control-Max-Age", 24 * 60 * 60 * 1000);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
 
   next()
