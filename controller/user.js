6e9ca9489c55d85593ed8e74d4bfb46e59ac3513
Fxkhalil0 <fxkhalil0@gmail.com>
2024-02-11 23:10:04 +0200

    ..

--- controller/user.js
+++ controller/user.js
@@ -321,17 +321,6 @@ async function sendCareerWelcomeEmail(userData) {
 }
 appregister.use(cors({ maxAge: 24 * 60 * 60 * 1000, origin: "https://www.lvw.live", exposedHeaders: '*', credentials: true, preflightContinue: true }));
 
-appregister.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://www.lvw.live');
-    res.header({ "Access-Control-Allow-Credentials": true });
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
-    res.header("Access-Control-Max-Age", 24 * 60 * 60 * 1000);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
-
-    next()
-})
-
 appregister.post("/addnew", async (req, res, next) => {
     const { role, firstName, lastName, email, phone, birthDate } = req.body;
     if (!firstName) {
@@ -367,7 +356,16 @@ appregister.post("/addnew", async (req, res, next) => {
             res.send(newuser);
         }
     }
-    next();
 });
+appregister.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://www.lvw.live');
+    res.header({ "Access-Control-Allow-Credentials": true });
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
+    res.header("Access-Control-Max-Age", 24 * 60 * 60 * 1000);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
+
+    next()
+})
 
 module.exports.appregister = appregister;
