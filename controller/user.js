c7e2a905132aeac6fdc456952719f05fbb7217a7
Fxkhalil0 <fxkhalil0@gmail.com>
2024-02-20 22:41:54 +0200

    ..

--- controller/user.js
+++ controller/user.js
@@ -4,6 +4,8 @@ const express = require("express")
 const cors = require("cors");
 const multer = require("multer");
 const path = require('path');
+const fs = require('fs');
+
 const { user } = require("../models/user");
 
 appregister = express()
@@ -42,8 +44,15 @@ const transporter = nodemailer.createTransport({
 
 // Function to send a welcome email
 async function sendUserWelcomeEmail(userData) {
-    try {
-        const info = await transporter.sendMail({
+  try {
+      fs.readFile(path.join(__dirname, "index.html"), { encoding: "utf-8" }, function (err, html) {
+          if (err) {
+              console.error("Error reading HTML file:", err);
+              return;
+          }
+
+          // Create mail options
+          const mailOptions = {
             from: '"LVW Tours" <fatmakhalilba@gmail.com>',
             to: userData.email,
             subject: "Welcome to LVW Tours Community",
@@ -84,203 +93,21 @@ async function sendUserWelcomeEmail(userData) {
                 cid: 'web'
             },
         ],
-            html: `<!DOCTYPE html>
-            <html lang="en">
-            
-            <head>
-              <meta charset="UTF-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-                integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-                crossorigin="anonymous" referrerpolicy="no-referrer" />
-              <title>Email Temp</title>
-              <style>
-                * {
-                  margin: 0;
-                  padding: 0;
-                  box-sizing: border-box;
-                }
-            
-                a {
-                  text-decoration: none;
-                  color: #ffffff;
-                }
-            
-                @media (max-width: 1100px) {
-                  footer {
-                    padding: 60px 0 !important;
-                  }
-                }
-            
-                @media (max-width: 1000px) {
-                  .container>h1 {
-                    font-size: 30px !important;
-                  }
-            
-                  .container>p {
-                    font-size: 20px !important;
-                  }
-                }
-            
-                @media (max-width: 800px) {
-                  .container>h1 {
-                    font-size: 28px !important;
-                  }
-            
-                  .container>p {
-                    font-size: 16px !important;
-                  }
-            
-                  .container>h2 {
-                    font-size: 20px !important;
-                  }
-                }
-            
-                @media (max-width: 600px) {
-                  footer {
-                    padding: 35px 0 !important;
-                  }
-                }
-            
-                @media (max-width: 500px) {
-                  .container>h1 {
-                    font-size: 22px !important;
-                  }
-            
-                  .container>p {
-                    font-size: 13px !important;
-                  }
-            
-                  .container>h2 {
-                    font-size: 16px !important;
-                  }
-            
-                  footer {
-                    padding: 50px 0 !important;
-                  }
-                }
-            
-                @media (max-width: 400px) {
-                  .container>h1 {
-                    font-size: 18px !important;
-                  }
-            
-                  .container>p {
-                    font-size: 13px !important;
-                  }
-            
-                  .container>h2 {
-                    font-size: 16px !important;
-                  }
-            
-                  .email {
-                    width: 45px !important;
-                  }
-            
-                  .web {
-                    width: 46.5px !important;
-                  }
-                }
-            
-                @media (max-width: 351px) {
-                  .container>h1 {
-                    font-size: 15px !important;
-                  }
-            
-                  .container>p {
-                    font-size: 13px !important;
-                  }
-            
-                  .container>h2 {
-                    font-size: 16px !important;
-                  }
-            
-                  footer {
-                    padding: 30px 0 !important;
-                  }
-            
-                  .facebook {
-                    width: 30px !important;
-                  }
-            
-                  .instagram {
-                    width: 19px !important;
-                    margin-right: 13px !important;
-                  }
-            
-                  .linkedin {
-                    width: 19px !important;
-                    margin-right: 11px !important;
-                  }
-            
-                  .youtube {
-                    width: 24px !important;
-                    margin-top: 5px !important;
-                  }
-            
-                  .email {
-                    width: 40px !important;
-                  }
-            
-                  .web {
-                    width: 42.5px !important;
-                  }
-                }
-              </style>
-            </head>
-            
-            <body style="background-color: #12243a; height: 100vh; font-family: 'Lato', sans-serif;">
-              <div class="main">
-                <div class="container" style="width: 85%; margin: 0 auto;">
-                  <div style="width: 100%; display:flex; justify-content: center !important; align-items:center; text-align:center; padding: 60px 0;">
-                    <img style="max-width: 100%; height: auto;" src="cid:logo" alt="Live Virtual World" />
-                  </div>
-                  <h1 style="text-align: center; font-size: 40px; font-weight: 600; color: #ffffff;">Welcome to the LVW Tours Community!</h1>
-                  <p style="color: #ffffff; font-weight: unset; font-size: 25px; margin-top: 50px; margin-bottom: 30px;">
-                    Welcome to LVW Tours – your gateway to unforgettable live journeys!
-                  </p>
-                  <p style="color: #ffffff; font-weight: unset; font-size: 25px; margin-bottom: 50px;">
-                    We've received your information, and you'll be the first to know about
-                    our upcoming launch. Get ready for exclusive updates as we embark on
-                    this journey together.
-                  </p>
-                  <h2 style="color: #ffffff; font-weight: 100; line-height: 33px;">
-                    Sincerely,<br/>
-                    LVW Team
-                  </h2>
-                </div>
-              </div>
-              <footer style="margin-top: 52px; background-color: #ff2629; padding: 52px 0; position: absolute; bottom: 0; width: 100%;">
-                <div class="container" style="width: 85%; margin: 0 auto;">
-                  <div style="display:flex; justify-content:space-between; align-items:center;">
-                    <div class="left__side" style="width: 80%; display:flex; align-items:center;">
-                      <a href="https://www.facebook.com/story.php?story_fbid=406443851756849&substory_index=898726195041079&id=100071737136498"
-                        target="_blank"><img class="facebook" style="width: 40px;" src="cid:face" /></a>
-                      <a href="https://www.instagram.com/live_virtual_world?igsh=MzRlODBiNWFlZA==" target="_blank"><img
-                          class="instagram" style="width: 25px; margin-right: 15px; margin-top: 8px;" src="cid:insta" /></a>
-                      <a href="https://www.linkedin.com/company/live-virtual-world" target="_blank"><img class="linkedin"
-                          style="width: 25px; margin-right: 13px; margin-top: 8px;" src="cid:link" /></a>
-                      <a href="https://www.youtube.com/@livevirtualworld7827?si=EnL3pBzNfYYrxW8W" target="_blank"><img
-                          class="youtube" style="width: 30px; margin-top: 7px;" src="cid:you" /></a>
-                    </div>
-                    <div class="right__side" style="width: 20%; display:flex; align-items:center; justify-content: center;">
-                      <a style="border: 1px solid #ffffff; border-radius: 50%; margin-right: 10px;margin-top: -4px; margin-bottom: 9px;" href="mailto:Info@lvw.live"><img
-                          class="email" style="width: 55px; padding: 10px; margin-top: 7px;" src="cid:email" /></a>
-                      <a style="border: 1px solid #ffffff; border-radius: 50%;margin-top: -4px; margin-bottom: 9px;" href="https://www.lvw.live/" target="_blank"><img
-                          class="web" style="width: 55px; padding: 10px; margin-bottom: -5px;" src="cid:web" /></a>
-                    </div>
-                  </div>
-                </div>
-              </footer>
-            </body>
-            
-            </html>`,
-        });
+            html: html, // Use the HTML content read from index.html
+          };
 
-        console.log("Welcome email sent to: %s", userData?.email);
-    } catch (error) {
-        console.error("Error sending welcome email:", error);
-    }
+          // Send the email
+          transporter.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                  console.error("Error sending email:", error);
+              } else {
+                  console.log("Email sent: " + info.response);
+              }
+          });
+      });
+  } catch (error) {
+      console.error("Error sending welcome email:", error);
+  }
 }
 async function sendCareerWelcomeEmail(userData) {
     try {
